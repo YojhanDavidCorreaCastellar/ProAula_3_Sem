99eda31423553256148f47628a7bf20516a3f2a3
YojhanDavidCorreaCastellar <yojhan.correacastellar@unicolombo.edu.co>
2025-05-07 15:53:21 -0500

    primer commit

---
+++ src/main/java/model/Vuelo.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author YOJHAN DAVID
+ */
+public class Vuelo {
+    private int codigo;
+    private String origen;
+    private String destino;
+    private String fechaHora;
+    private String duracion;
+    private String precio;
+    private String avion;
+    
+    public Vuelo(){
+        
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(String fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public String getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(String duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getAvion() {
+        return avion;
+    }
+
+    public void setAvion(String avion) {
+        this.avion = avion;
+    }
+    
+}
